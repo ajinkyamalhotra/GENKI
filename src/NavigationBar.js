974d52efddc2600ccb4ae8f123cad21433c55f67
Joshua Shewmaker <31637431+Jshewmaker1@users.noreply.github.com>
2019-01-27 08:02:04 -0800

    Appearance change to bar.

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -10,20 +10,26 @@ class NavigationBar extends Component {
       <Router>
       <div className="navigation-bar">
         {
-          <Button.Group floated='right'>
-          <Link to='/public/HomePage.html'>
-            <Button>Home Page</Button>
-          </Link>
-          <Link to='/public/Profile.html'>
-            <Button>Profile</Button>
-          </Link>
-          <Link to='/public/Game.html'>
-            <Button>Game</Button>
-          </Link>
-          <Link to='/public/LoginPage.html'>
-            <Button>Log Out</Button>
-          </Link>
-          </Button.Group>
+          <Table color='grey' inverted>
+            <Table.Header>
+              <Table.Row>
+                <Button.Group floated='right'>
+                  <Link to='/public/HomePage.html'>
+                    <Button>Home Page</Button>
+                  </Link>
+                  <Link to='/public/Profile.html'>
+                    <Button>Profile</Button>
+                  </Link>
+                  <Link to='/public/Game.html'>
+                    <Button>Game</Button>
+                  </Link>
+                  <Link to='/public/LoginPage.html'>
+                    <Button>Log Out</Button>
+                  </Link>
+                </Button.Group>
+              </Table.Row>
+            </Table.Header>
+          </Table>
         }
       </div>
     </Router>
