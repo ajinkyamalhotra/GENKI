4e4fdcdb4a873a9d85ab88345fe34fd4a72b5c1a
Joshua Shewmaker <31637431+Jshewmaker1@users.noreply.github.com>
2019-01-27 16:44:58 -0800

    image not working, but still works as a placeholder for now.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import NavigationBar from './NavigationBar';
-import Login from './login';
+import Game from './game';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavigationBar />
-        <Login />
+        <Game />
       </div>
     );
   }
--- src/game.js
+++ src/game.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react';
+import {Image} from 'semantic-ui-react';
 
 class Game extends Component {
-  
+  render(){
+    return(
+      <div className='game' style={{marginLeft: '40%', marginTop: '15%'}}>
+        <Image src='./images/logo.png' size='large'/>
+      </div>
+    )
+  }
 }
+
+export default Game;
