b24f802ac293c0ce3797d3e0a90e49b10a77834d
Amr Masoud <47014816+amrmasoud@users.noreply.github.com>
2019-02-04 15:35:11 -0800

    Added image to class inside a container

--- src/Profile.js
+++ src/Profile.js
@@ -3,42 +3,31 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Button, Form, Container, Header} from 'semantic-ui-react';
 
 import logo from './logo.png'; // Tell Webpack this JS file uses this image
-/*
+
+console.log(logo); // /logo.84287d09.png
+
 class Profile extends Component
 {
   render()
   {
-    return
-    (
-
-      <div className='loginForm' style={{borderStyle: 'solid', width: '30%', margin: 'auto', marginTop: '15%'}}>
+    return(
+      <div className='ProfileForm' style={{borderStyle: 'solid', width: '50%', margin: 'auto', marginTop: '5%'}}>
         <Container>
-          <Form size='huge'>
-            <Header as='h1'>Log In</Header>
-            <label>Username</label>
-            <Form.Input placeholder='Username' name='username' width='12'/>
-            <label>Password</label>
-            <Form.Input placeholder='Password' name='password' width='12'/>
-            <Button>Login</Button>
-            <Button floated='right'>Register</Button>
-          </Form>
+          <img src={logo} alt="Logo" style={{ width: '80%', margin: 'auto', marginLeft: '10%'}}/>
+          <h1 style={{ margin: 'auto', marginLeft: '30%'}}> Team GENKI</h1>
         </Container>
       </div>
-      //<h1> hi </h1>
-      //ReactDOM.render({ProfileImage()}, document.getElementById('root'));
     )
   }
-}*/
+}
 
-console.log(logo); // /logo.84287d09.png
-
-function ProfileImage()
+/*function ProfileImage()
 {
   // Import result is the URL of your image
   return <img src={logo} alt="Logo" />;
 
 }
 
-//export default Profile;
+export default ProfileImage;*/
 
-export default ProfileImage;
+export default Profile;
