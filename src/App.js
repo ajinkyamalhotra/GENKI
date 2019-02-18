ec2b5509c5f726c850bc68717be50d9dfffeba26
woodchuck33 <22991547+woodchuck33@users.noreply.github.com>
2019-02-18 15:34:31 -0800

    Moved the routing logic to the App and started user logic.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation';
 //import NavigationHome from './components/NavigationHome';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import Game from './Game';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Progress from './components/Progress';
+import SignUp from './components/SignUp';
+import SignUpConfirmation from './components/SignUpConfirmation';
 
 
 /**
@@ -9,11 +17,42 @@ import './App.css';
  * component only.
  */
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUser: false,
+      username: ''
+    }
+
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  handleLogin(username) {
+    this.setState({isUser: true, username});
+  }
+
   render() {
     return (
       // Render the Navigation component
       <div className="App">
         <Navigation />
+        <div>
+          {this.state.isUser ? 'Logged In as ' + this.state.username:'Not Logged in'}
+        </div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/Game' component={Game} />
+
+          <Route
+            exact
+            path='/Login'
+            render={() => (<Login onLogin={this.handleLogin}/>)} />
+
+          <Route exact path='/Profile' component={Profile} />
+          <Route exact path='/Progress' component={Progress} />
+          <Route exact path='/SignUp' component={SignUp} />
+          <Route exact path='/SignUpConfirmation' component={SignUpConfirmation} />
+        </Switch>
       </div>
     );
   }
