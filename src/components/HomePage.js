1a567d97953a5645ec3d55723fe5e529bd7ec40c
woodchuck33 <22991547+woodchuck33@users.noreply.github.com>
2019-02-27 18:05:22 -0800

    Changes to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
+import { Button, Card } from 'semantic-ui-react'
 
 class HomePage extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      userType: props.userType,
+      pendingTeachers: []
+    }
+    if (props.userType === 'admin') {
+      this.getPendingTeachers();
+    }
+    this.getPendingTeachers = this.getPendingTeachers.bind(this);
+    this.PendingCards = this.PendingCards.bind(this);
+  }
 
-    this.getPendingTeachers();
+  componentDidMount() {
+    console.log(this.state);
   }
 
   getPendingTeachers() {
-    var pendingTeachers;
     console.log('fetching pending teachers');
     fetch('/pending')
       .then(response => response.json())
-      .then(response => console.log(response));
+      .then(response => {
+        this.setState({pendingTeachers: response.pendingTeachers})
+      });
+  }
+
+  PendingTeacherCard(user) {
+    return(
+      <Card key={user.email}>
+        <Card.Content>
+          <Card.Header>{ user.firstName + ' ' + user.lastName }</Card.Header>
+          <Card.Meta>New Instructor</Card.Meta>
+          <Card.Description>
+            {user.firstName + ' wants to be added as a new Instructor.'}
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button basic color='green'>
+              Approve
+            </Button>
+            <Button basic color='red'>
+              Decline
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    );
+  }
+
+  PendingCards(props) {
+    if (this.state.userType === 'admin') {
+      var cards = props.userList.map((user) => this.PendingTeacherCard(user));
+      return (
+        <Card.Group>
+          {cards}
+        </Card.Group>
+      )
+    } else {
+      return null;
+    }
+
   }
 
   render() {
     return (
       <div>
         This is the Home Page!
+        <this.PendingCards userList={this.state.pendingTeachers} />
       </div>
     )
   }
