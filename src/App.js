7ad5f74852ee71801e4859eaa6d25b68ee8b969e
woodchuck33 <22991547+woodchuck33@users.noreply.github.com>
2019-03-04 20:44:50 -0800

    Routing change so that all components have user info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation';
 import { Switch, Route } from 'react-router-dom';
+import Routes from './routing/Routes';
 import HomePage from './components/HomePage';
 import Game from './Game';
 import Login from './components/Login';
@@ -21,10 +22,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isUser: false,
+      isAuthenticated: false,
     }
 
     this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  userHasAuthenticated(authenticated) {
+    this.setState({ isAuthenticated: authenticated });
   }
 
   /**
@@ -37,14 +42,20 @@ class App extends Component {
     let firstName = userAttributes.name;
     let lastName = userAttributes.family_name;
 
-    this.setState({ isUser: true, username, email, firstName, lastName, userType });
+    this.setState({ isAuthenticated: true, username, email, firstName, lastName, userType });
     console.log(username + ' ' + email + ' ' + firstName + ' ' + lastName + ' ' + userType);
   }
 
-
-   render() {
-     const userType = this.state.userType;
-     return (
+  render() {
+    const childProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      userHasAuthenticated: this.userHasAuthenticated,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      userType: this.state.userType
+    };
+    return (
         // Render the Navigation component
         <div className="App">
           <Navigation />
@@ -52,42 +63,7 @@ class App extends Component {
             {this.state.isUser ?
               'Logged In as ' + this.state.userType  : 'Not Logged in'}
           </div>
-
-          <Switch>
-            <Route
-              exact
-              path='/'
-              render={() => (
-                <HomePage userType={this.state.userType} />)} />
-
-            <Route exact path='/Game' component={Game} />
-
-            <Route
-              exact
-              path='/Login'
-              render={(props) => (
-                <Login {...props} onLogin={this.handleLogin}/>)} />
-
-            <Route
-              exact
-              path='/Profile'
-              render={(userType) => (
-                <Profile userType={userType} />)} />
-
-            <Route exact path='/Progress' component={Progress} />
-
-            <Route
-              exact
-              path='/SignUp'
-              render={(props) => (
-                <SignUp {...props}/>)} />
-
-            <Route
-              exact
-              path='/SignUpConfirmation'
-              component={SignUpConfirmation} />
-
-          </Switch>
+          <Routes childProps={childProps}/>
         </div>
       );
   }
