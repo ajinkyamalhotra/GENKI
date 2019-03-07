92dbae1bc40b343d0680ecbe5a1f1f5617f3f087
Jose Prieto <joseprieto@csus.edu>
2019-03-06 17:15:53 -0800

    Remove comments from story.js

--- src/story/story.js
+++ src/story/story.js
@@ -367,57 +367,36 @@ let story = [
     spriteLeft: managerNeutralGif,
     text: "はい、「リトル・アジア」です。"
   },
-  /*{
-    text: "Yes, this is Little Asia."
-  },*/
   {
     speaker: john,
     spriteRight: johnStudyingGif,
     spriteLeft: managerNeutral,
     text: "私、ジョン・ワンと申します。アルバイト募集の広告を見たんですが。"
   },
-  /*{
-    text: "My name is John Wang. I saw your classified ad."
-  },*/
   {
     speaker: manager,
     spriteLeft: managerNeutralGif,
     spriteRight: johnStudying,
     text: "そうですか。じゃあ、会って、話しましょうか。今日店に来られますか。"
   },
-  /*{
-    text: "I see. Well, shall we meet and talk? Can you come to the store today?"
-  },*/
-
   {
     speaker: john,
     spriteRight: johnStudyingGif,
     spriteLeft: managerNeutral,
     text: "今日はちょっと行けないんですが、あしたなら行けると思います。"
   },
-  /*{
-    text: "I cannot come today, but if it's tomorrow, I think I can come."
-  },*/
-
   {
     speaker: manager,
     spriteLeft: managerNeutralGif,
     spriteRight: johnStudying,
     text: "そうですか。今日はだめですか。じゃあ、あしたの一時ごろはどうですか。"
   },
-  /*{
-    text: "I see. Today is no good...All right, how about one o' clock tomorrow?
-  "},*/
-
   {
     speaker: john,
     spriteRight: johnStudyingGif,
     spriteLeft: managerNeutral,
     text: "一時ですね。わかりました。"
   },
-  /*{
-    text: "One o' clock. Okay, I've got it."
-  },*/
 
   //Scene 2, John arrives for interview at Little Asia
   {
@@ -433,55 +412,31 @@ let story = [
     spriteLeft: managerNeutral,
     text: "ワンさんはどうしてこのアルバイトに興味があるんですか。"
   },
-  /*{
-    text: "Mr. Wang, why are you interested in this job?"
-  },*/
   {
     speaker: john,
     spriteRight: johnWaiter,
     text: "おもしろそうですから。いろいろな人に会えるし、日本語も使えるし。"
   },
-  /*{
-    text: "It seems interesting. I can meet various people; I can also use Japanese... "
-  },*/
   {
     speaker: manager,
     text: "レストランで働いたことがありますか。"
   },
-  /*{
-    text: "Have you worked at a restaurant before?"
-  },*/
-
   {
     speaker: john,
     text: "はい。ウェイターならしたことがあります。"
   },
-  /*{
-    text: "Yes. As a waiter, I have."
-  },*/
-
   {
     speaker: manager,
     text: "あしたから始められますか。"
   },
-  /*{
-    text: "Can you start tomorrow?"
-  },*/
-
   {
     speaker: john,
     text: "はい。よろしくお願いします。"
   },
-  /*{
-    text: "Yes. I'll do my best."
-  },*/
   {
     speaker: manager,
     text: "がんばってください。"
   },
-  /*{
-    text: "Good luck."
-  },*/
 
   //Scene 3, John is on the job at the restaurant
   {
@@ -499,49 +454,28 @@ let story = [
     spriteRight: johnWaiter,
     text: "いらっしゃいませ。あ、山下先生。"
   },
-  /*{
-    text: "Oh, Professor Yamashita."
-  },*/
   {
     speaker: yamashita,
     spriteLeft: yamashitaNeutral,
     spriteRight: johnWaiter,
     text: "ジョンさん。ここでアルバイトをしているんですか。"
   },
-  /*{
-    text: "John, do you work here."
-  },*/
   {
     speaker: john,
     text: "ええ。一週間に三日働いています。"
   },
-  /*{
-    text: "Yes, I work three days a week."
-  },*/
-
   {
     speaker: yamashita,
     text: "そうですか。どれがおいしいですか。"
   },
-  /*{
-    text: "I see. Which one is good?"
-  },*/
-
   {
     speaker: john,
     text: "このカレーが一番人気がありますよ。"
   },
-  /*{
-    text: "This curry is the most popular one.
-  },*/
-
   {
     speaker: yamashita,
     text: "おいしそうですね。じゃあ、食べてみます。"
   },
-  /*{
-    text: "It looks good. Well, I will try this one."
-  },*/
 
   //////////////////////////Chapter 14//////////////////////////////////////////
 
@@ -559,25 +493,16 @@ let story = [
     spriteLeft: maryNeutral,
     text: "バレンタインのプレゼントは何がいいと思いますか。"
   },
-  /*{
-    text: "What do you think is for a Valentine’s present?"
-  },*/
   {
     speaker: michiko,
     spriteRight: michikoNeutral,
     text:
       "そうですね。たけしさんはいつも同じセーターを着ているから、セーターをあげたらどうですか。"
   },
-  /*{
-    text: "Well, Takeshi always the same sweater, so why don’t you give him a sweater?"
-  },*/
   {
     speaker: mary,
     text: "それはいいかもしれませんね。"
   },
-  /*{
-    text: "That might be a good idea."
-  },*/
 
   //Scene 5
   {
@@ -593,49 +518,29 @@ let story = [
     spriteRight: marySitting,
     text: "たけしくん、はい、これ。"
   },
-  /*{
-    text: "Takeshi, this is for you."
-  },*/
   {
     speaker: takeshi,
     spriteLeft: takeshiSweater,
     text: "えっ、ぼくに？どうもありがとう。開けてもいい？"
   },
-  /*{
-    text: "For me? Thank you. May I open it?"
-  },*/
   {
     speaker: mary,
     text: "うん。"
   },
-  /*{
-    text: "Yes."
-  },*/
-
   {
     speaker: takeshi,
     text:
       "わあ、いいね、このセーター。こんなのがほしかったんだ。メアリーが編んだの？"
   },
-  /*{
-    text: "Wow, this is a nice sweater! I’ve wanted one like this.  Did you knit it Mary?"
-  },*/
-
   {
     speaker: mary,
     text: "うん、小さいかもしれないから着てみて。"
   },
-  /*{
-    text: "Yes. It may be small, so please try it on."
-  },*/
 
   {
     speaker: takeshi,
     text: "ちょうどいいよ。ありがとう。"
   },
-  /*{
-    text: "It fits perfectly. Thank you."
-  },*/
 
   //Scene 6
   {
@@ -651,63 +556,38 @@ let story = [
     spriteLeft: johnNeutral,
     text: "暖かそうなセーターですね。"
   },
-  /*{
-    text: "Your sweater looks warm."
-  },*/
   {
     speaker: takeshi,
     spriteRight: takeshiInSweater,
     text: "これ、メアリーがくれたんです。"
   },
-  /*{
-    text: "Mary gave me this."
-  },*/
   {
     speaker: john,
     text:
       "よく似合っていますよ。ぼくも彼女がほしいなあ。ロバートさんはチョコレートを十個ももらったんですよ。"
   },
-  /*{
-    text: "It looks good on you. I want a girlfriend, too. You know, Robert got as many as ten chocolates."
-  },*/
   {
     speaker: takeshi,
     text: "へえ、すごいですね。ジョンさんは？"
   },
-  /*{
-    text: "Hah, that’s incredible. How about you John?"
-  },*/
 
   {
     speaker: john,
     text: "ぼくは一個しかもらえませんでした。大家さんから。さびしいなあ。"
   },
-  /*{
-    text: "Yes. I only got one. From my landlady. How sad?"
-  },*/
 
   {
     speaker: takeshi,
     text: "でも、ロバートさんはホワイトデーが大変ですよ。"
   },
-  /*{
-    text: "But Robert will probably have a tough day on White Day."
-  },*/
   {
     speaker: john,
     text: "ホワイトデー？"
   },
-  /*{
-    text: "White Day?"
-  },*/
-
   {
     speaker: takeshi,
     text: "ええ、男の人は三月十四日にお返しをしなきゃいけないんですよ。"
   },
-  /*{
-    text: "Yes, boys have to return the favor on March 14th."
-  },*/
 
   ////////////////////Chapter 15 below//////////////////////////////////////
 
@@ -725,54 +605,33 @@ let story = [
     spriteLeft: maryTalking,
     text: "たけしくん、今度の休み、予定ある？"
   },
-  /*{
-    text: "Takeshi, do you have any plans for the holidays?"
-  },*/
   {
     speaker: takeshi,
     spriteRight: takeshiTalking,
     text: "ううん。別に。どうして？"
   },
-  /*{
-    text: "Not really. Why?"
-  },*/
   {
     speaker: mary,
     text: "みちこさんの長野のうちに行こうと思ってるんだけど、一緒に行かない？"
   },
-  /*{
-    text: "I am thinking of going to Michiko’s home in Nagano. Do you want to go?"
-  },*/
   {
     speaker: takeshi,
     text: "いいの？"
   },
-  /*{
-    text: "Is it okay?"
-  },*/
 
   {
     speaker: mary,
     text: "うん。みちこさんが、「たけしくんも誘って」と言ってたから。"
   },
-  /*{
-    text: "Yes, Michiko told me to invite you."
-  },*/
 
   {
     speaker: takeshi,
     text: "じゃあ、行く。電車の時間、調べておくよ。"
   },
-  /*{
-    text: "Then, I will go. I will check the train schedule."
-  },*/
   {
     speaker: mary,
     text: "ありがとう。じゃあ、私、みちこさんに電話しておく。"
   },
-  /*{
-    text: "Thanks. I will call Michiko."
-  },*/
 
   //Scene 8
   {
@@ -788,42 +647,26 @@ let story = [
     spriteRight: takeshiTalking,
     text: "早く着いたから、ちょっと観光しない？"
   },
-  /*{
-    text: "Since we got here early, do you want to do a little sightseeing?"
-  },*/
   {
     speaker: mary,
     spriteLeft: maryTalking,
     text: "うん。どこに行く？"
   },
-  /*{
-    text: "Yes. Where shall we go?"
-  },*/
   {
     speaker: takeshi,
     spriteRight: takeshiTalking,
     sprite: takeshiTemple,
     text: "善光寺はどう？有名なお寺だよ。"
   },
-  /*{
-    text: "How about Zenkoji Temple? It’s a famous temple."
-  },*/
   {
     speaker: mary,
     text: "そうだね。昼ご飯は何にする？"
   },
-  /*{
-    text: "Sounds good. What shall we eat for lunch?"
-  },*/
-
   {
     speaker: takeshi,
     sprite: takeshiSoba,
     text: "長野はそばがおいしいから、そばを食べようよ。"
   },
-  /*{
-    text: "Soba noodles in Nagano are delicious, so let’s eat soba."
-  },*/
 
   //Scene 9
   {
@@ -841,48 +684,29 @@ let story = [
     spriteLeft: maryTalking,
     text: "すみません、善光寺に行くバスはどれですか。"
   },
-  /*{
-    text: "Excuse me, which bus goes to Zenkoji Temple?"
-  },*/
   {
     speaker: infoAgent,
     sprite: infoAgentNeutral,
     text: "善光寺なら、十一番のバスですよ。"
   },
-  /*{
-    text: "For Zenkoji, it’s bus number 11."
-  },*/
   {
     speaker: takeshi,
     text: "ありがとうございます。この地図、もらってもいいですか。"
   },
-  /*{
-    text: "Thank you very much. Can I have this map? "
-  },*/
   {
     speaker: infoAgent,
     text:
       "ええ、どうぞ。それから、これ、美術館の割引券ですが、よかったらどうぞ。"
   },
-  /*{
-    text: "Yes, And these are discount tickets for the museum. Please take them, if you like."
-  },*/
-
   {
     speaker: mary,
     text: "これ、東山魁夷の絵がある美術館ですね。あした行く予定なんです。"
   },
-  /*{
-    text: "This is the museum that has paintings of Higashiyama Kaii, isn’t it? We are planning to go tomorrow. Thank you."
-  },*/
   {
     speaker: infoAgent,
     text: "気をつけて。",
     jumpTo: "title-screen"
   }
-  /*{
-    text: "Have a safe trip."
-  },*/
 
   //end of game
 ];
