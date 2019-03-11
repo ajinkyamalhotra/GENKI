90b917f147e2b46a0b7d2891fe2ef9aec5bfbebb
Joshua Shewmaker <31637431+Jshewmaker1@users.noreply.github.com>
2019-03-11 14:46:25 -0700

    announcement backend

--- genki-backend-api/getAnnouncements.js
+++ genki-backend-api/getAnnouncements.js
@@ -4,34 +4,34 @@ export function main (event, context, callback) {
   const data = JSON.parse(event.body);
   let docClient = new AWS.DynamoDB.DocumentClient();
   let table = "Announcements";
-  let message = data.message;
+  let index = "ClassID-index";
   let classID = data.classID;
-  let date = data.date;
   let params = {
-    TableName:table,
-    Item:{
-      "ClassID": classID,
-      "Date": date,
-      "Message": message
-    }
+    TableName: table,
+    IndexName: index,
+    KeyConditionExpression: "ClassID = :c_ID",
+    ExpressionAttributeValues: {
+      ":c_ID": classID
+    },
+    ProjectionExpression: "Message, Date"
   };
 
   console.log("Creating Announcement");
-  docClient.update(params, function(err, result){
+  docClient.query(params, function(err, result){
     if(err){
       const response = {
         statusCode: 500,
         body: JSON.stringify({ status: false })
       };
       callback(null, response);
-      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+      console.error("Unable to query item. Error JSON:", JSON.stringify(err, null, 2));
     } else {
       const response = {
         statusCode: 200,
         body: JSON.stringify({ status: true })
       };
       callback(null, response);
-      console.log("Added item:", JSON.stringify(result, null, 2));
+      console.log("Queried item:", JSON.stringify(result, null, 2));
     }
   });
 
