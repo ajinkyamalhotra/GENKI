415b262ef304f813e3f299b677dfe98723154154
Amr Masoud <47014816+amrmasoud@users.noreply.github.com>
2019-03-22 19:51:02 -0700

    API files for Save/Load

--- genki-backend-api/saveGame.js
+++ genki-backend-api/saveGame.js
@@ -1,21 +1,40 @@
 import AWS from 'aws-sdk';
 
-const cognito = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
+//const cognito = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
 
 export function main (event, context, callback) {
-  var data;
+  const data = JSON.parse(event.body);
   // Set response headers to enable CORS (Cross-Origin Resource Sharing)
   const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Credentials': true
   };
-  console.log('received POST');
-  try {
+
+let table = "User_Save";
+let username = data.Username;
+let dateCreated = data.DateCreated;
+let slotNumber = data.SlotNumber;
+let saveString = data.SaveString; // stringified from Game.js
+
+let params = {
+  TableName:table,
+  Item:{
+    "Username": username,
+    "DateCreated": dateCreated,
+    "SlotNumber": slotNumber,
+    "SaveString": saveString,
+  }
+}
+
+  console.log('Adding new user save');
+  console.log(data);
+
+  /*try {
     data = JSON.parse(event.body);
     var params = {
       saveString: data.saveString,
       createdAt: Date.now()
-    };
+    };*/
 
   } catch (e) { // Error occured during parsing
     console.log('Issue with parsing');
