95135d1fd86b3696d4075ef3db430f72e2d51ced
Joshua Shewmaker <31637431+Jshewmaker1@users.noreply.github.com>
2019-03-26 13:32:17 -0700

    student add class form completed.

--- src/components/StudentAddClassForm.js
+++ src/components/StudentAddClassForm.js
@@ -2,16 +2,46 @@ import React, {Component} from 'react';
 import { Button, Header, Form, Grid, Input} from 'semantic-ui-react';
 import { Icon, Divider} from 'semantic-ui-react';
 import { API } from 'aws-amplify';
+import '../styles/AddClassForm.css';
 
 class StudentAddClassForm extends Component {
   constructor(props){
     super(props);
     this.state = {
-      ClassID: ''
+      classID: ''
     }
 
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.FormField = this.FormField.bind(this);
+    this.ButtonOptions = this.ButtonOptions.bind(this);
   }
 
+  componentWillUnmount(){
+    this.setState = {
+      classID: ''
+    }
+  }
+  /**
+   * Used to make the form a controlled component.
+   * Will be called when the user types in the username or password textbox.
+   * @param event           Represents the change coming from the form.
+   */
+  handleChange = (event) => {
+    // Getting the name of the state variable which is changing.
+    // Corresponds to the name property of the field.
+    const key = event.target.name;
+    this.setState({[key]: event.target.value});
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      await this.addUserClass();
+    } catch (e) {
+      alert(e);
+    }
+  }
   /**
    * Function which invokes the classAdd API to create/update user with the
    * entered classID
@@ -23,15 +53,100 @@ class StudentAddClassForm extends Component {
     let params = {
       body: {
         username: this.props.username,
-        classID: this.state.secretID
+        classID: this.state.classID
       }
     }
     return API.post(apiName, path, params);
   }
+  /**
+   * Returns a Form.Field Semantic UI component.
+   * @param props       Includes the color, name, and label for the field.
+   */
+  FormField(props) {
+    let stateField;
+
+    if(props.label === 'Class ID'){
+      stateField=this.state.classID;
+    }
+    return (
+      <Form.Field>
+        <this.Label color={props.color}
+                    name={props.name}
+                    label={props.label}/>
+
+        <Input      value={stateField}
+                    name={props.label}
+                    onChange={this.handleChange}
+                    type={props.type}
+                    placeholder={props.placeholder}/>
+      </Form.Field>
+    )
+  }
+
+  /**
+   * Returns a custom label.
+   * @param props           Includes the color, name, and label.
+   */
+  Label(props) {
+    return (
+      <label size='huge'>
+        <Icon color={props.color} name={props.name} />
+          {props.label}
+      </label>
+    )
+  }
+
+  /**
+   * Returns the submission button.
+   * This button is disabled until all of the necessary inputs have been
+   * given.
+   */
+  SubmitButton() {
+    const {className, section, semester, classTime} = this.state;
+    const isEnabled = className;
+    console.log(className);
+
+    return(
+      <Button size='big'
+              compact fluid
+              color='orange'
+              type='Submit'
+              disabled={!isEnabled}
+              onClick={this.handleSubmit}>
+        Submit
+      </Button>
+    )
+  }
 
   render(){
     return(
-      
+      <div className='page'> {
+        <Grid  padded='vertically'>
+            <Divider />
+            <Grid.Row>
+                <Grid.Column width={4} className='addClassForm'>
+                    <Divider />
+                    <Grid.Row>
+                        <Header as='h1' inverted color='orange'>
+                        Add to Class</Header>
+                    </Grid.Row>
+                    <Divider />
+                    <Form inverted>
+                        <this.FormField
+                          color='orange'
+                          label='Class ID'
+                          type='text'
+                          placeholder='Class ID'
+                        />
+                        <Divider />
+                        <this.SubmitButton/>
+                    </Form>
+            </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    }
+
+    </div>
     )
   }
 
