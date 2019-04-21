ddb17c6f901e5ea5b272b03a03f6f6f61128dec4
ajinkyamalhotra <ajinkyamalhotra73@gmail.com>
2019-04-21 11:22:31 -0700

    Making position of bottom bar fixed at the bottom-right of homepage

--- src/components/BreadCrumbBottomBar.js
+++ src/components/BreadCrumbBottomBar.js
@@ -8,7 +8,7 @@ export default class BreadCrumbBottomBar extends Component {
   render() {
     return (
       <Segment basic
-               style={{position: 'absolute', bottom: '15px', right: '15px'}}>
+               style={{position: 'fixed', bottom: '15px', right: '15px'}}>
         <FAQs />
         <Aboutus />
       </Segment>
