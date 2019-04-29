48802524691cb65a293747f3af88e8fd93040eee
ajinkyamalhotra <ajinkyamalhotra73@gmail.com>
2019-04-28 18:46:40 -0700

    Including the bottom bar in App.js to make it persistent between pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navigation from './components/Navigation';
 import Routes from './routing/Routes';
 import { Auth } from 'aws-amplify';
 import { Loader } from 'semantic-ui-react';
+import BreadCrumbBottomBar from './components/BreadCrumbBottomBar';
 
 import './App.css';
 
@@ -92,6 +93,7 @@ class App extends Component {
                   <Navigation childProps={childProps} handleLogout={this.handleLogout} />
                   <Routes childProps={childProps} />
                 </React.Fragment>)}
+                <BreadCrumbBottomBar />
         </div>
       );
   }
