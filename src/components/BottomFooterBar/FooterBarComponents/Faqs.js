1ced4c23a19343d87743fcab1dbb38ace604800d
Joshua Shewmaker <31637431+Jshewmaker1@users.noreply.github.com>
2019-05-05 15:11:41 -0700

    added 5 questions and answers.

--- src/components/BottomFooterBar/FooterBarComponents/Faqs.js
+++ src/components/BottomFooterBar/FooterBarComponents/Faqs.js
@@ -10,15 +10,42 @@ import '../../../styles/Faqs.css';
  */
 let QuestionsAndAnswers = [
   {Question:"How do I sign up for a new account?",
-   Answer:"Click on the sign up button in the top bar," +
+   Answer:"Click on the sign up button in the top bar, " +
       "fill up the required information and click Sign up.\n"+
-      "Check your email inbox used to signup for the account and" +
+      "Check your email inbox used to signup for the account and " +
       "enter the received confirmation code to create your account."},
 
   {Question:"How do I log into an existing account?",
-   Answer:"Click on the log in button in the top bar," +
+   Answer:"Click on the log in button in the top bar, " +
      "enter your credentials and click Submit."},
 
+  {Question:"How do I enroll in a class?",
+    Answer:"From the Home page, click on Enroll In A Class and " +
+      "enter the Class ID that your instructor provided to you " +
+      "into the form. Only students can enroll in a class."},
+
+  {Question:"How can I contact my professor?",
+    Answer:"From the Home page, click on the class of the instructor " +
+      "you want to contact. In the Class Information tab, click the button " +
+      "next to your instructor's name. This will open your default email " +
+      "program with your instructor's email address in the \"To\" box."},
+
+  {Question:"How can I view announcements?",
+    Answer:"From the Home page, click on the class whose announcements " +
+      "you want to view. Click on the Class Announcements tab to see " +
+      "that class' announcements."},
+
+  {Question:"How can I create an announcement?",
+    Answer:"From the Home page, click on the class where you want to create " +
+      "an announcement. Click on the Class Announcements tab to view that " +
+      "class' current announcements. Click the Add Announcement button " +
+      "to bring up a form with 2 entries, Title and Announcement. " +
+      "Fill out the form and click Create Announcement. "},
+
+  {Question:"What code do I need to give to my students to enroll?",
+    Answer:"After creating a class, click on the new class on the Home page. " +
+      "In the class information tab, the bottom entry is Class ID. " +
+      "Send that code to your students so they can enroll in your class."},
   ];
 
 /**
