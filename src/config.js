93fa68c02f7ffe0fe8a5ff2603d0535bf8e11bfa
Joshua Shewmaker <31637431+Jshewmaker1@users.noreply.github.com>
2019-06-02 10:02:26 -0700

    Config.js updated

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 export default {
   s3: {
     REGION: "us-west-2",
-    BUCKET: "genki-vn-beta"
+    BUCKET: "genki-vn-prod"
   },
   apiGateway: {
     REGION: "us-west-2",
-    URL: "https://8hylre3t7j.execute-api.us-west-2.amazonaws.com/prod"
+    URL: "https://y4sre7t6ke.execute-api.us-west-2.amazonaws.com/prod/"
   },
   cognito: {
     REGION: "us-west-2",
-    USER_POOL_ID: "us-west-2_WV9ZxGPoJ",
-    APP_CLIENT_ID: "2sncsnmssjvov5k5t7ea261q6m",
-    IDENTITY_POOL_ID: "us-west-2:f6900b66-9dd7-4f1f-beaf-5dbbd076c008"
+    USER_POOL_ID: "us-west-2_1T0BDHNGx",
+    APP_CLIENT_ID: "6ji1h1t25kr6ns64luv61rtf1",
+    IDENTITY_POOL_ID: "us-west-2:cff82784-8ea9-431b-9cd1-1c6c3ec2d499"
   },
   API_NAME: "genki-vn-prod"
 };
